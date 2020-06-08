3715adeda71f4ed3f85afc2022a9e32b657e0eef
kndb_vortex_TTCY <kamganguifo@gmail.com>
2020-06-09 00:10:16 +0100

    Suppression des bug et ajout d'une couleur sur les boutons

--- calculator.py
+++ calculator.py
@@ -41,7 +41,7 @@ class Calculatrice(QMainWindow):
 							"cos" : (3, "droite", lambda a: math.cos(a)), 
 							"cos⁻¹" : (3, "droite", lambda a : math.acos(a)),
 							"ln" : (3, "droite" , lambda a : math.log(a)),
-							"log" : (3, "droite" , lambda a : math.log10(a))
+							"log" : (3, "droite" , lambda a : math.log10(a)),
 							"pgcd" : (3, "droite" , lambda a, b : math.gcd(a,b)), 
 							"ppcm" : (3, "droite", lambda a, b : int(a * b / math.gcd(a,b))),
 							"sin" : (3, "droite", lambda a: math.sin(a)), 
@@ -66,19 +66,33 @@ class Calculatrice(QMainWindow):
 		self.ui.bouton_division.clicked.connect(lambda : self.verif_operator("/"))
 		self.ui.bouton_delete.clicked.connect(lambda : self.del_button())
 		self.ui.bouton_virgule.clicked.connect(lambda : self.insert_virgule())
-		self.ui.bouton_egal.clicked.connect(lambda: self.set_label_calcul(self.ui.label_operation.text()))
+		self.ui.bouton_egal.clicked.connect(lambda: self.gestion_egal() )
+		self.ui.bouton_ans.clicked.connect(lambda : self.gestion_ans() )
 		self.ui.bouton_parenthese_ouvrante.clicked.connect(lambda : self.parenthese_bouton("("))
 		self.ui.bouton_parenthese_fermante.clicked.connect(lambda : self.parenthese_bouton(")"))
 		#Bouton function 
 		self.ui.bouton_racin_carre.clicked.connect(lambda : self.racine_carre())
 		self.ui.bouton_puissance.clicked.connect(lambda : self.verif_operator("^"))
 		self.ui.bouton_puissance10.clicked.connect(lambda : self.petite_verif_pour_puissance10())
-		self.ui.comboBox_fonctions.currentIndexChanged.connect(lambda : self.gestion_fonction(self.ui.comboBox_fonctions.currentIndex()))
+		self.ui.comboBox_fonctions.activated.connect(lambda : self.gestion_fonction(self.ui.comboBox_fonctions.currentIndex()))
 
 
 	def gestion_fonction(self, index):
 		if index != 0:
 			self.ui.label_operation.setText(self.ui.label_operation.text() + self.ui.comboBox_fonctions.currentText()[:-2])
+
+
+	def gestion_fonction_1(self, event):
+		if self.ui.comboBox_fonctions.currentIndex() != 0:
+			self.ui.label_operation.setText(self.ui.label_operation.text() + self.ui.comboBox_fonctions.currentText()[:-2])
+
+
+	def gestion_egal(self):
+		self.ans = self.set_label_calcul(self.ui.label_operation.text())
+
+
+	def gestion_ans (self):
+		self.ui.label_operation.setText(self.ui.label_operation.text() + "ans")
 
 	
 	def racine_carre(self):
@@ -107,10 +121,26 @@ class Calculatrice(QMainWindow):
 
 	def del_button(self):
 		''' bouton supprime un caractère '''
-		if len(self.ui.label_operation.text()) != 0:
-			self.ui.label_operation.setText(self.ui.label_operation.text()[:-1])
-		self.set_label_calcul(self.ui.label_operation.text())
+		if len(self.ui.label_operation.text()) > 0:
+			if self.ui.label_operation.text()[-1]  in self.liste_chiffres + [k for k in self.liste_operateurs] + ["(", ")"]:
+				self.ui.label_operation.setText(self.ui.label_operation.text()[:-1])
+			else :
+				val_label_operation = self.ui.label_operation.text()
 
+				while len(val_label_operation) > 0:
+					if val_label_operation[-1] not in self.liste_chiffres + [k for k in self.liste_operateurs] + ["(", ")"] :
+						val_label_operation = val_label_operation[:-1]
+					else :
+						break
+				self.ui.label_operation.setText(val_label_operation)
+			if len(self.ui.label_operation.text()) > 0:
+				if self.ui.label_operation.text()[-1]  in self.liste_chiffres + [k for k in self.liste_operateurs] + ["(", ")"]:
+					self.set_label_calcul(self.ui.label_operation.text())
+			else :
+				self.ui.label_resultat.setText("")
+		else:
+			self.ui.label_resultat.setText("")
+		
 
 	def test(self, num):
 		'''Ajoute un chiffre à l'écran'''
@@ -158,7 +188,8 @@ class Calculatrice(QMainWindow):
 			if (not virgule_present and not op_present) :
 				self.ui.label_operation.setText(self.ui.label_operation.text() + ".")
 			elif (not virgule_present and op_present):
-				self.ui.label_operation.setText(self.ui.label_operation.text() + "0.")	
+				if self.ui.label_operation.text()[-1] in self.liste_chiffres :
+					self.ui.label_operation.setText(self.ui.label_operation.text() + ".")
 
 
 	#Shunting Yard Algorithm
@@ -176,7 +207,7 @@ class Calculatrice(QMainWindow):
 					while operateurs :
 						a = (operateurs[-1] in liste_total_operateurs)
 						if a : 
-							if (liste_total_operateurs[element][0] <= liste_total_operateurs[operateurs[-1]][0] and liste_total_operateurs[element][1] == liste_total_operateurs[operateurs[-1]][1]) or \
+							if (liste_total_operateurs[element][0] < liste_total_operateurs[operateurs[-1]][0] and liste_total_operateurs[element][1] == liste_total_operateurs[operateurs[-1]][1]) or \
 								  (liste_total_operateurs[element][0] <= liste_total_operateurs[operateurs[-1]][0] and liste_total_operateurs[element][1] != liste_total_operateurs[operateurs[-1]][1]) :
 								  liste_final.append(operateurs.pop())
 							else :
@@ -195,10 +226,11 @@ class Calculatrice(QMainWindow):
 						else:
 							operateurs.pop()
 							break
+			elif element == "ans":
+				liste_final.append(self.ans)
 			#Si c'est un nombre on l'ajoute sans faire d'histoire à la file
 			else :
 				liste_final.append(element)
-			#print(liste_final, operateurs)
 		while operateurs :
 			if operateurs[-1] not in ["(", ")"]:
 				liste_final.append(operateurs.pop())
@@ -236,12 +268,10 @@ class Calculatrice(QMainWindow):
 					final.append(chaine_caractere[i])
 
 				elif chaine_caractere[i] == "√":
-					print("Racine carrée")
 					if i > 0 :
 						if chaine_caractere[i - 1] in self.liste_chiffres :
 							final.append("x")
 					final.append(chaine_caractere[i])
-					print(final)
 
 				else :
 					if i == 0  or (i > 0 and chaine_caractere[i - 1] == "(" ):
@@ -264,7 +294,7 @@ class Calculatrice(QMainWindow):
 			elif chaine_caractere[i] in self.liste_chiffres:
 				#le nombre peut avoir plusieurs chiffres d'où la présence de cette variable var qui nous aidera à rassembler ceux-ci
 				val = ""
-				while  i < len(chaine_caractere) and not (chaine_caractere[i] in [k for k in self.liste_operateurs ] + ["(", ")"]) : 
+				while  i < len(chaine_caractere) and  (chaine_caractere[i] in self.liste_chiffres + ["."]) : 
 					val = val + chaine_caractere[i]
 					i = i + 1
 
@@ -276,6 +306,9 @@ class Calculatrice(QMainWindow):
 				i = i + 1
 			#S'il s'agit d'un caractère implicitement une fonction
 			else :
+				if i > 0 :
+					if chaine_caractere[i - 1] in self.liste_chiffres :
+						final.append("x")
 				op = ""
 				while i < len(chaine_caractere):
 					if chaine_caractere[i] in ["(", ")", ","] + [k for k in self.liste_operateurs] + self.liste_chiffres:
@@ -284,13 +317,11 @@ class Calculatrice(QMainWindow):
 						op = op + chaine_caractere[i]
 					i = i + 1
 				final.append(op) 
-		print(final)
 		return final
 
 
 	def calcul_post_fixe(self, final):
 			i = 0
-			print(final)
 			while i < len(final):
 				if  final[i] in liste_operateurs:
 					if final[i] == "√":
@@ -336,13 +367,24 @@ class Calculatrice(QMainWindow):
 					return  final[0] 				
 
 	def set_label_calcul(self, chaine):
-		liste_charac = self.extraction_pour_calcul_post_fixe(self.ui.label_operation.text())
-		liste_charac = self.transform_post_fixe(liste_charac)
-		self.ui.label_resultat.setText( str(self.calcul_post_fixe(liste_charac)))
-		
+		try:
+
+			liste_charac = self.extraction_pour_calcul_post_fixe(self.ui.label_operation.text())
+			liste_charac = self.transform_post_fixe(liste_charac)
+			result = self.calcul_post_fixe(liste_charac)
+			self.ui.label_resultat.setText( str(result))
+			return result
+		except ValueError :
+			self.ui.label_resultat.setText("Math Error")
+		except OverflowError :
+			self.ui.label_resultat.setText("∞")
+		except TypeError:
+			self.ui.label_resultat.setText("Synthax erreur")
+		except ZeroDivisionError :
+			self.ui.label_resultat.setText("∞")
 
 
-	
+
 
 def main():
 	app = QApplication(sys.argv)
@@ -351,155 +393,5 @@ def main():
 	sys.exit(app.exec())
 
 	
-	#Shunting Yard Algorithm
-def transform_post_fixe(items):
-	operateurs = []
-	liste_final = deque()
-	for element in items:
-		if element in liste_operateurs:
-			print ("ok ......",operateurs)
-			if not operateurs :
-				operateurs.append(element)
-			elif operateurs[-1] == "(" :
-				operateurs.append(element)
-			else:
-				while operateurs :
-					print(liste_final,operateurs)
-					a = (operateurs[-1] in liste_operateurs)
-					if a : 
-						if (liste_operateurs[element][0] <= liste_operateurs[operateurs[-1]][0] and liste_operateurs[element][1] == liste_operateurs[operateurs[-1]][1]) or \
-							  (liste_operateurs[element][0] <= liste_operateurs[operateurs[-1]][0] and liste_operateurs[element][1] != liste_operateurs[operateurs[-1]][1]) :
-							  liste_final.append(operateurs.pop())
-						else :
-							break
-					else:
-						break
-				operateurs.append(element)
-				print(operateurs[-1])
-				print(liste_final,operateurs)
-		#Si c'est une parenthèse ouvrante on l'ajoute si c'est une parenthèse fermante on dépile les opérateurs jusqu'à la prochaine parenthèse ouvrante
-		elif element in ["(", ")"] :
-			print("yeah")
-			if element == "(":
-				operateurs.append(element)
-			elif element == ")":
-				print("dépilage",liste_final ,operateurs, element)
-				while operateurs:
-					print("good .............",operateurs[-1])
-					if operateurs[-1] != "(":
-						print("good")
-						liste_final.append(operateurs.pop())
-					else:
-						operateurs.pop()
-						break
-		#Si c'est un nombre on l'ajoute sans faire d'histoire à la file
-		else :
-			print("c'est un nombre")
-			liste_final.append(element)
-		#print(liste_final, operateurs)
-	while operateurs :
-		if operateurs[-1] not in ["(", ")"]:
-			liste_final.append(operateurs.pop())
-		else :
-			operateurs.pop()
-	print(liste_final, operateurs)
-	return list(liste_final)
-
-
-def extraction_pour_calcul_post_fixe( chaine_caractere):
-	
-	if len(chaine_caractere) == 0:	
-			return ""	
-	else :
-		uniquement_fait_op = True
-		for i in chaine_caractere :
-
-			if i not in  [k for k in liste_operateurs] + ["(" , ")"]: 	
-				uniquement_fait_op = False;
-				break
-		if uniquement_fait_op:
-			return ""
-
-	if chaine_caractere[-1] in liste_operateurs:
-		chaine_caractere = chaine_caractere[:-1]
-	es_negatif = False
-	#final est une liste qui contiendra l'opération post fixée
-	final = []
-	i = 0
-	num = True
-	#On parcourt l'expression que l'on veut transformer 
-	while i  < len(chaine_caractere):
-		# s'il s'agit d'une simple opération (+, -, /, *) on ajoute le caractère dans operateurs 
-		if chaine_caractere[i] in liste_operateurs:
-			if chaine_caractere[i] != "-" :
-				final.append(chaine_caractere[i])
-			else :
-				if i == 0  or (i > 0 and chaine_caractere[i - 1] == "(" ):
-					final.append(0.0)
-					es_negatif = True
-				final.append(chaine_caractere[i])
-			num = False
-			i = i + 1
-		#les s'il s'agit d'une parenthèse  
-		elif chaine_caractere[i] in ["(", ")"]:
-			if chaine_caractere[i] == "(":
-				if chaine_caractere[i-1] in liste_chiffres and i > 0:
-					final.append("x")
-				final.append("(")
-			else:
-				final.append(chaine_caractere[i])
-			num = False
-			i = i + 1
-		#Si c'est un nombre 
-		else :
-			#le nombre peut avoir plusieurs chiffres d'où la présence de cette variable var qui nous aidera à rassembler ceux-ci
-			val = ""
-			while  i < len(chaine_caractere) and not (chaine_caractere[i] in [k for k in liste_operateurs ] + ["(", ")"]) : 
-				val = val + chaine_caractere[i]
-				i = i + 1
-
-			final.append(float(val))
-			if es_negatif :
-				es_negatif = False
-			num = True
-	return final
-
-
-def calcul(a, b, op):
-	if op == "+":
-		return a + b
-	elif op == "-":
-		return a - b;
-	elif op == "x":
-		return a * b;
-	elif op == "/":
-		return a / b;
-	elif op == "^":
-		return a ** b
-
-def calcul_post_fixe(final):
-		i = 0
-		while i < len(final):
-			if  final[i] in liste_operateurs:
-				print("un operateur ...", final[i], final)
-				a = final[i-2]
-				b = final[i-1]
-				a = calcul(a,b,final[i])
-				b = i-2
-				for j in range(3):
-					final.pop(b)
-
-				final.insert(b, a)
-				i = 0
-			else:
-				print("un nombre .....")
-				i = i + 1
-
-		if len(final) > 0:
-			if len(final) > 1:
-				return "Syntax error"
-			else:
-				return  final[0] 				
-
 if __name__ == "__main__":
 	main()
